1b62842cd9a0631f7ca1061fdf5eef81d895e198
Ksusha <o.derechinskaya@gmail.com>
2023-07-02 21:54:52 +0300

    выполнила задачи: 1-1.4; 2; 3; 4;

--- src/main/java/homeWork/HW1.java
+++ src/main/java/homeWork/HW1.java
@@ -1,5 +1,6 @@
 package homeWork;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,7 +11,7 @@ public class HW1 {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        /*1.1
+        /* HW 1.1
         driver.get("https://uhomki.com.ua/koshki/1074/");
         Thread.sleep(4000);
         WebElement element = driver.findElement(By.className("userbar__button-text"));
@@ -18,27 +19,53 @@ public class HW1 {
         Thread.sleep(4000);
         driver.quit();*/
 
-        /*1.2
+        /* HW 1.2
         driver.get("https://uhomki.com.ua/koshki/1074/");
         Thread.sleep(4000);
         driver.findElement(By.linkText("Вход")).click();
         Thread.sleep(4000);
         driver.quit();*/
 
-        /*1.3
+        /* HW 1.3
         driver.get("https://uhomki.com.ua/koshki/1074/");
         Thread.sleep(4000);
         driver.findElement(By.xpath("//*[@class='userbar__button-text']")).click();
         Thread.sleep(4000);
         driver.quit();*/
 
-        /*1.4
+        /* HW 1.4
         driver.get("https://uhomki.com.ua/koshki/1074/");
         Thread.sleep(4000);
         driver.findElement(By.xpath("//span[text()='Вход']")).click();
         Thread.sleep(4000);
         driver.quit();*/
 
-        System.out.println();
+        /* HW 2
+        driver.get("https://uhomki.com.ua/koshki/1074/");
+        Thread.sleep(4000);
+        driver.findElement(By.xpath
+                ("//div[@class='footer__heading']/..//child::a")).click();
+        Thread.sleep(4000);
+        driver.quit();*/
+
+        /* HW 3
+        driver.get("https://dan-it.com.ua/");
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//a[text()='Українська']")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//div[@class='banner__btns']/a")).click();
+        Thread.sleep(4000);
+        driver.quit();*/
+
+        /* HW 4
+        driver.get("https://uhomki.com.ua/koshki/1074/");
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("Рыбы")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("Аквариумы")).click();
+        Thread.sleep(2000);
+        driver.quit();*/
+
+
 }
 }
