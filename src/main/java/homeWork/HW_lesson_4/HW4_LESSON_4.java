4f0030ca911a5e06bf450cc404d0a822d14344bf
Ksusha <o.derechinskaya@gmail.com>
2023-07-11 23:37:28 +0300

    выполнила ДЗ

--- src/main/java/homeWork/HW_lesson_4/HW4_LESSON_4.java
+++ src/main/java/homeWork/HW_lesson_4/HW4_LESSON_4.java
@@ -17,6 +17,25 @@ public class HW4_LESSON_4 {
         driver.get("https://uhomki.com.ua/ru");
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("Хорек");
+        Thread.sleep(8000);
         element.sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath
+                        ("//a[@href='/ru/vitaminy-beafar-pasta-malt-s-dvoynym-deystviem-dlya-khorkov-100g/']"))
+                .click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath
+                        ("//span[@class='comparison-button__icon']/*/*"))
+                .click();
+        driver.findElement(By.name("q")).sendKeys("Медведь");
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//input[@class='search__input']")).sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//a[@title='Белый медведь 6х5х6см']")).click();
+        driver.findElement(By.xpath("//span[@class='comparison-button__icon']/*/*")).click();
+        Thread.sleep(6000);
+        driver.findElement(By.xpath("//span[@class='comparison-view__button-text']")).click();
+        Thread.sleep(6000);
+        driver.findElement(By.xpath("//div[@class='compare-close j-close']/*")).click();
+        Thread.sleep(4000);
+        driver.quit();
     }
 }
