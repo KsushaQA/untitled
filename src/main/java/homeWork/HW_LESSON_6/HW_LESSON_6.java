15814afadc905822e08022f90f1aa350d4e45605
Ksusha <o.derechinskaya@gmail.com>
2023-07-17 01:32:48 +0300

    ДЗ6 - добавила.

--- src/main/java/homeWork/HW_LESSON_6/HW_LESSON_6.java
+++ src/main/java/homeWork/HW_LESSON_6/HW_LESSON_6.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.Set;
 
@@ -50,7 +51,37 @@ public class HW_LESSON_6 {
         driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
         driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("Voronec");
         driver.findElement(By.xpath("//input[@type='submit']")).click();
-        System.out.println(driver.findElement(By.tagName("p")).getText());
-
+        Thread.sleep(6000);
+        System.out.println(driver.findElement
+                (By.cssSelector(".w3-panel.w3-pale-yellow.w3-leftbar.w3-border-yellow")).getText());
+        Set<String> windowHandles = driver.getWindowHandles();
+        String currentWindow = driver.getWindowHandle();
+        String newWindow = "https://www.guinnessworldrecords.com/account/register?";
+        for (String window : windowHandles) {
+            if (!window.equals(currentWindow)) {
+                newWindow = window;
+                break;
+            }
+        }
+        driver.switchTo().window(newWindow);
+        driver.findElement(By.id("LastName")).sendKeys("Voronec");
+        driver.findElement(By.id("FirstName")).sendKeys("Oksana");
+        driver.findElement(By.id("DateOfBirthDay")).sendKeys("10");
+        driver.findElement(By.id("DateOfBirthMonth")).sendKeys("12");
+        driver.findElement(By.id("DateOfBirthYear")).sendKeys("1999");
+        Select country = new Select(driver.findElement(By.id("Country")));
+        country.selectByVisibleText("Ukraine");
+        Thread.sleep(6000);
+        driver.findElement(By.id("State")).sendKeys("Dnipro");
+        driver.findElement(By.id("EmailAddress")).sendKeys("OKv.@gmail.com");
+        driver.findElement(By.id("ConfirmEmailAddress")).sendKeys("OKv.@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("12345678");
+        driver.findElement(By.id("ConfirmPassword")).sendKeys("12345679");
+        driver.findElement(By.id("ConfirmPassword")).sendKeys("12345679");
+        //section[@class='form-section form-half']
+        driver.findElement(By.xpath("//section[@class='form-section form-half']")).click();
+        Thread.sleep(2000);
+        System.out.println(driver.findElement
+                (By.xpath("//span[@data-valmsg-for='ConfirmPassword']")).getText());
     }
 }
